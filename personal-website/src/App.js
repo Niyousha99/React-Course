c2054c0f2f3a4bf341164f14fed994b8bcad23b7
Niyousha99 <niyousha.raeesinejad@ucalgary.ca>
2021-02-19 09:22:03 -0700

    Fixed layout of images

--- personal-website/src/App.js
+++ personal-website/src/App.js
@@ -1,22 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
-import './Oatmeal/Oatmeal';
-import oatmeal_1 from './images/oatmeal_1.jpg';
-import oatmeal_2 from './images/oatmeal_2.jpg';
-import oatmeal_3 from './images/oatmeal_3.jpg';
-import oatmeal_4 from './images/oatmeal_4.jpg';
-import oatmeal_5 from './images/oatmeal_5.jpg';
-import oatmeal_6 from './images/oatmeal_6.jpg';
+import Oatmeal from './Oatmeal/Oatmeal';
 
 class App extends Component {
   state = {
-    oatmealBowls: [
-      { name: "Strawberry Delight", img: "oatmeal_1" },
-      { name: "Fire and Water", img: "oatmeal_2" },
-      { name: "Banana Cinnamon", img: "oatmeal_3" },
-      { name: "Chocolate Chip Berry", img: "oatmeal_4" },
-      { name: "Berry Seed Mountain", img: "oatmeal_5" },
-      { name: "Perfect Pear", img: "oatmeal_6" },
+    oatmealInfo: [
+      { name: "Strawberry Delight", imageName: "oatmeal_0" },
+      { name: "Syrupy Coconut", imageName: "oatmeal_1" },
+      { name: "Banana Cinnamon", imageName: "oatmeal_2" },
+      { name: "Chocolate Chip Berry", imageName: "oatmeal_3" },
+      { name: "Berry Seed Mountain", imageName: "oatmeal_4" },
+      { name: "We Make a Perfect Pear", imageName: "oatmeal_5" },
     ]
   }
 
@@ -32,16 +26,9 @@ class App extends Component {
             className="mainButton">SHOW ME</button>
         </div>
 
-        <section className="images">
-          <img src={oatmeal_1}></img>
-          <img src={oatmeal_2}></img>
-          <img src={oatmeal_3}></img>
+        <Oatmeal
+          oatmeals={this.state.oatmealInfo} />
 
-          <img src={oatmeal_4}></img>
-          <img src={oatmeal_5}></img>
-          <img src={oatmeal_6}></img>
-
-        </section>
       </div>
     );
   }
--- personal-website/src/Oatmeal/Oatmeal.js
+++ personal-website/src/Oatmeal/Oatmeal.js
@@ -1,2 +1,40 @@
+import React from 'react';
+import oatmeal_0 from './images/oatmeal_0.jpg'
+import oatmeal_1 from './images/oatmeal_1.jpg'
+import oatmeal_2 from './images/oatmeal_2.jpg'
+import oatmeal_3 from './images/oatmeal_3.jpg'
+import oatmeal_4 from './images/oatmeal_4.jpg'
+import oatmeal_5 from './images/oatmeal_5.jpg'
 import './Oatmeal.css'
 
+const Oatmeal = (props) => {
+
+    let oatmealAlts = {
+        "oatmeal_0": oatmeal_0,
+        "oatmeal_1": oatmeal_1,
+        "oatmeal_2": oatmeal_2,
+        "oatmeal_3": oatmeal_3,
+        "oatmeal_4": oatmeal_4,
+        "oatmeal_5": oatmeal_5,
+    }
+
+    const imgStyle = {
+        width: "30%",
+        height: "500px",
+        margin: "1rem"
+    };
+
+    return (
+        <section className="OatmealSection">
+            {
+                props.oatmeals.map((oatmeal, i) => {
+                    return <img src={oatmealAlts[`oatmeal_${i}`]} key={i} style={imgStyle} />;
+                })
+            }
+
+        </section>
+
+    )
+};
+
+export default Oatmeal;
